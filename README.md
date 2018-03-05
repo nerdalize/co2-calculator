c230a04ee6ada52e0405d899f17d9752331d2e76
borismattijssen <borismattijssen@gmail.com>
2018-03-05 11:03:13 +0100

    Use argument parser

--- README.md
+++ README.md
@@ -3,22 +3,32 @@ Nerdalize is building a different cloud. Rather than putting thousands of server
 
 This Docker image calculates the amount of CO2 saved per year by Nerdalize. By taking an amount of households as input, it outputs the following:
 
-1. Total CO2 savings in kilograms and how many traveling kilometers this corresponds to for different ways of transportation. This is both written to stdout and plotted in a bar chart.
+1. Total CO2 savings in kilograms and how many traveling kilometers this corresponds to for different ways of transportation.
 2. If a file `flights.csv` is present, a bar chart showing how many airplane trips you could make, based on the different trips specified in the file.
 
 
 ## Usage
 Executing just the python file:
 ```
-python co2_calc.py [no-of-households] [output-folder] [input-folder]
+usage: co2_calc.py [-h] [--input INPUT] [--output OUTPUT] [households]
+
+positional arguments:
+  households            number of households (default: 5)
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --input INPUT, -i INPUT
+                        input folder (default: /input)
+  --output OUTPUT, -o OUTPUT
+                        output folder (default: /output)
 ```
 
 Executing as a Docker container:
 ```
-docker run -v "$PWD":/input -v "$PWD":/output nerdalize/co2-calculator [no-of-households]
+docker run -v "$PWD":/input -v "$PWD":/output nerdalize/co2-calculator [households]
 ```
 
 Or simply execute the following if you're not interested in output files:
 ```
-docker run nerdalize/co2-calculator [no-of-households]
+docker run nerdalize/co2-calculator [households]
 ```
--- co2_calc.py
+++ co2_calc.py
@@ -1,21 +1,25 @@
 import sys
 import os.path
+import argparse
 import numpy as np
 from transport import Transport
 import utils
 
-households = 5 if len(sys.argv) < 2 else int(sys.argv[1])
-output_folder = '/output' if len(sys.argv) < 3 else sys.argv[2]
-input_folder = '/input' if len(sys.argv) < 4 else sys.argv[3]
+# Parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('households', nargs='?', type=int, default=5, help='number of households (default: %(default)s)')
+parser.add_argument('--input', '-i', default='/input', help='input folder (default: %(default)s)')
+parser.add_argument('--output', '-o', default='/output', help='output folder (default: %(default)s)')
+args = parser.parse_args()
 
 def km_to_world(km):
     return km / 40000
 
-co2_kg = households * 3000
+co2_kg = args.households * 3000
 transport = Transport(co2_kg)
 
 # 1. Print traveling kilometers per transport type.
-print("For {} households, Nerdalize reduces CO2 emissions by {:,d} kg per year.".format(households, co2_kg))
+print("For {} households, Nerdalize reduces CO2 emissions by {:,d} kg per year.".format(args.households, co2_kg))
 print("")
 print("This is equivalent to (per person):")
 tmpl = "{} {:,.0f} Kilometers by {}, or {:.1f} times around the world 🌍"
@@ -23,13 +27,14 @@ print(tmpl.format("🚗", transport.car(), "car", km_to_world(transport.car())))
 print(tmpl.format("🚆", transport.train(), "train", km_to_world(transport.train())))
 print(tmpl.format("✈️ ", transport.airplane(), "airplane", km_to_world(transport.airplane())))
 
+# define input and output files
+input_file = args.input + "/flights.csv"
+output_file = args.output + '/flights.png'
+
 # 2. Read input file and plot amount of trips that could be made for amount of households.
-input_file = input_folder + "/flights.csv"
 if os.path.isfile(input_file):
     try:
-        utils.mkdir(output_folder)
-        output_file = output_folder + '/flights.png'
-
+        utils.mkdir(args.output)
         x = []
         y = []
         flights = np.genfromtxt(input_file, delimiter=';', names=True, dtype=None, encoding='utf-8')
@@ -40,13 +45,13 @@ if os.path.isfile(input_file):
         utils.plot(x, y,
             'Flights',
             'Number of trips',
-            'The amount of trips you can make by ✈️ per year, for {} households.'.format(households),
+            'The amount of trips you can make by ✈️ per year, for {} households.'.format(args.households),
             output_file)
 
         print("\n>> Created flights plot {}".format(output_file))
     except OSError as e:
         _, strerror = e.args
-        print("Failed to create output folder {}: {}".format(output_folder, strerror))
+        print("Failed to create output folder {}: {}".format(args.output, strerror))
         sys.exit(1)
     except:
         print("Unexpected error:", sys.exc_info()[0])
